7e34764bb53a78ca2c1d5e83e5b6c2c2060f18ae
Larry <larry@larrys-macbook-pro.local>
2018-07-22 20:17:54 -0700

    Initial Commit

---
+++ AI_Message/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AI_Message
+//
+//  Created by Larry  on 7/22/18.
+//  Copyright © 2018 Larry . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
